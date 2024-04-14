346b76143fbd65faff8802a46ba74c04dd1c63cd
Steven Engler <smengler3@gmail.com>
2024-04-14 18:39:51 -0400

    Added LibreJS license to theme switcher

--- static/js/theme-switcher.js
+++ static/js/theme-switcher.js
@@ -1,3 +1,6 @@
+// license information for librejs extension users
+// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt "Expat License (sometimes called MIT Licensed)"
+
 // in Firefox we need to add a new css style with document.write rather than modifying the href
 // of the existing one, otherwise the screen will flash white while loading on dark themes
 var theme_css_elem = document.getElementById('theme_css');
@@ -38,3 +41,5 @@ function toggle_theme(){
 	}
 	update_toggle_button();
 }
+
+// @license-end
